9d3882d2540454574505dedca5b469485345b3d4
匡莉 <20373335@buaa.edu.cn>
2023-01-31 16:28:53 +0800

    词频统计initail

---
+++ 2、下载pdf年报/2.1merge.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+pd.set_option('max_columns', 100)
+pd.set_option('max_colwidth', 100)
+
+df1=pd.read_excel('aim_data.xlsx',dtype={'code':str})
+df2=pd.read_excel('all_url_data.xlsx',dtype={'code':str})
+
+df3=pd.merge(df1,df2,on=['code','year'],how='left')
+
+df4=df3.loc[:,['code','firm','year','pdf_url']]
+df4.to_excel('merged_data.xlsx',index=False)
+
+print(df4)
